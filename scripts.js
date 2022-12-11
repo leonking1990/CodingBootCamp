1778cd84fb44b2deac5bb81e520e282ed3ae1a05
leonking1990 <leonking1990@gmail.com>
2022-12-10 22:04:48 -0800

    adding modification

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-ECHO is on. //first assigment for week 1
+// print Introduction
+console.log("Hello my name is Steve")
+
+let x = 6;
+
+console.log(x);
+
+let z = 5 + x;
+
+console.log(z);
+
+z = z+x;
+// result
+console.log(z);
